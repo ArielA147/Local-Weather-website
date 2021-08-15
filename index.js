f99ca96287d533ef33c4a2bdd42f7efb9b834b85
ArielA147 <ariel.147@gmail.com>
2021-08-15 23:17:13 +0300

    adding new filed of information (feels like), adding an event listener to the toggle, change reading from the api will be happning only when the user gives prem

--- index.js
+++ index.js
@@ -1,15 +1,16 @@
 function getData(){
     const url = 'https://weather-proxy.freecodecamp.rocks/api/current?lat=32.0958&lon=34.9522';
     fetch( url, {
-        method: 'GET',
+        method: 'GET'
     })
     .then(response => response.json())
     // .then(data => console.log(data)) // the output of the response json 
     .then(data => {
         // extractValues(data)
-        updateWeatherText(data)
-        // updateWeatherIcon(data)
+        getWeatherText(data)
+        getWeatherIcon(data)
         updateCurrentTemp(data)
+        updateFeelsLikeTemp(data)
     })
     .catch(error => {console.error('Error:', error)});
 }
@@ -18,22 +19,26 @@ function extractValues(data){
     console.log(data);
 }
 
-function updateWeatherText(data){
+function getWeatherText(data){
     document.getElementById('data-watertype').innerHTML = data.weather[0].description;
 }
 
-// TBD: this is not working !!
-function updateWeatherIcon(data){
-    document.getElementById("icon-watertype22").src=data.weather[0].icon;;
+function getWeatherIcon(data){
+    document.getElementById("icon-watertype22").src=data.weather[0].icon;
 }
 
 let tempCelsius;
+let feelsLiketempCelsius;
 
 function updateCurrentTemp(data){
     tempCelsius =  Math.round(data.main.temp) +'\xB0C';
     document.getElementById('data-temp').innerHTML = tempCelsius;
 }
 
+function updateFeelsLikeTemp(data){
+    feelsLiketempCelsius =  Math.round(data.main.feels_like) +'\xB0C';
+    document.getElementById('data-temp-feels-like').innerHTML = feelsLiketempCelsius;
+}
 
 function convertToF(celsius) {
     let fahrenheit = celsius.match(/\d+/)[0] * 9/5 + 32;
@@ -46,8 +51,27 @@ function toggle()
 
     if (document.querySelector(".toggle-btn").classList.contains('active')){
         document.getElementById("data-temp").innerHTML= convertToF(temp);
+        document.getElementById("data-temp-feels-like").innerHTML= convertToF(document.getElementById("data-temp-feels-like").innerHTML);
     }
     else {
         document.getElementById("data-temp").innerHTML= tempCelsius;
+        document.getElementById("data-temp-feels-like").innerHTML = feelsLiketempCelsius;
+    }
+}
+
+var btn = document.getElementById("use-location");
+
+// event lisnter on the button clicked
+btn.onclick = function(event){
+    if (event.target.checked){
+        getData() // extract the 
+        document.querySelector(".toggle-container").style.visibility = 'visible';
+    } else{
+        // hide all the data we extract from the API
+        document.getElementById("data-temp").innerHTML = "";
+        document.getElementById("data-temp-feels-like").innerHTML ="";
+        document.getElementById("data-watertype").innerHTML = "";
+        document.getElementById("icon-watertype22").src = "";
+        document.querySelector(".toggle-container").style.visibility = 'hidden';
     }
 }
